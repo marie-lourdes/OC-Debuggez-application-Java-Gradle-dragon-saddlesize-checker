11aca1a41ff1df589f4136b91af47156dfa1a802
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-03-15 19:25:51 +0100

    update target classe

--- src/main/java/com/openclassrooms/debugging/DragonSaddleSizeVerifier.java
+++ src/main/java/com/openclassrooms/debugging/DragonSaddleSizeVerifier.java
@@ -6,10 +6,12 @@ public class DragonSaddleSizeVerifier {
 
     public void verify(Double saddleSize) {
         if (null == saddleSize) {
+        	System.out.println("Taille de selle inattendue");
             throw new InvalidSaddleSizeException("Taille de selle nulle inattendue");
         }
 
         if (saddleSize <= 0) {
+        	System.out.println("Taille de selle inattendue:" + saddleSize);
             throw new InvalidSaddleSizeException("Taille de selle inattendue:" + saddleSize);
         }
     }
